1839d58f7c96693b12e4edda3210f28c2a1d30b2
gregoryekhator <41048560+gregoryekhator@users.noreply.github.com>
2023-07-13 08:45:32 +0100

    Update connect.js
    redacted

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',
-  password: 'Ge3318JJ',
+  password: 'xxxxxx',
   host: 'localhost',
   port: 5432,
   database: 'bank_account'
